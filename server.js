9b0432acacc81f5675fabc8e206fa5aee8ab7938
DineshNutheti <nanidinesh4203@gmail.com>
2024-06-11 19:35:51 +0530

    Uploading Code files

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Define the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
